9a1514960264726d1bc4fe7c54603018ae270c1d
sherlinci <aditya.suru95@gmail.com>
2019-09-09 17:55:20 -0400

    this is the updated version

--- bad_code_1.py
+++ bad_code_1.py
@@ -1,3 +1,3 @@
 if __name__ == "__main__":
-print 'I hate it when people call me "Fifa", that's my cat's name!'
+	print ('I hate it when people call me "Fifa", that\'s my cats name!')
 
--- bad_code_2.py
+++ bad_code_2.py
@@ -1,4 +1,4 @@
 if __name__ == "__main__":
     # For you're ouwn helth, Im cuting you of to a max of 4 beers.
-    for i in range(4, -1, -1):
-        print("%d bottles of beer on the wall, %d bottles of beer.  Take one down, pass it around, %d bottles of beer on the wall." % (i, i, i))
+    for i in range(4, 0, -1):
+        print("%d bottles of beer on the wall, %d bottles of beer.  Take one down, pass it around, %d bottles of beer on the wall." % (i, i, i-1))
